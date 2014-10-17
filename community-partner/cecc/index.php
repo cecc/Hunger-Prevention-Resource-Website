6ef173f6fc717faa282ee5de28447cf6712f992c
CesarOH <cesar.hernandez1094@gmail.com>
2014-10-17 11:49:06 -0400

    Update index.php

--- community-partner/cecc/index.php
+++ community-partner/cecc/index.php
@@ -61,19 +61,19 @@ include ('../../header.php');
         <div class="col-lg-4">
           <img class="img-circle" width="140px" height="140px" src="IMG_0532.jpg" alt="Generic placeholder image">
           <h2>Who We Are ...</h2>
-          <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
+          <p>We are the New Jersey Institute of Technology Civic Engagment Computer Center a division of the Career Development Services</p>
           <p><a class="btn btn-default" href="#row1" role="button">View details &raquo;</a></p>
         </div><!-- /.col-lg-4 -->
         <div class="col-lg-4">
           <img class="img-circle" width="140px" height="140px" src="IMG_0528.JPG" alt="Generic placeholder image">
-          <h2>Services</h2>
-          <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.</p>
+          <h2>What We Do...</h2>
+          <p>We create websites for non-profit organizations who need to have an internet presence</p>
           <p><a class="btn btn-default" href="#row2" role="button">View details &raquo;</a></p>
         </div><!-- /.col-lg-4 -->
         <div class="col-lg-4">
           <img class="img-circle" data-src="holder.js/140x140" alt="Generic placeholder image">
-          <h2>Contact Info</h2>
-          <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+          <h2>How to Get in Touch</h2>
+          <p>You may be able to contact us by phone at (973) 596-5401 or by email.</p>
           <p><a class="btn btn-default" href="#row3" role="button">View details &raquo;</a></p>
         </div><!-- /.col-lg-4 -->
       </div><!-- /.row -->
