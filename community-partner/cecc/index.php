5bf4451b0afcaf0ca241a0cbac45ac1b5531810f
CesarOH <cesar.hernandez1094@gmail.com>
2014-10-17 10:02:04 -0400

    Update index.php

--- community-partner/cecc/index.php
+++ community-partner/cecc/index.php
@@ -66,13 +66,13 @@ include ('../../header.php');
         </div><!-- /.col-lg-4 -->
         <div class="col-lg-4">
           <img class="img-circle" width="140px" height="140px" src="IMG_0528.JPG" alt="Generic placeholder image">
-          <h2>Heading</h2>
+          <h2>Services</h2>
           <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.</p>
           <p><a class="btn btn-default" href="#row2" role="button">View details &raquo;</a></p>
         </div><!-- /.col-lg-4 -->
         <div class="col-lg-4">
           <img class="img-circle" data-src="holder.js/140x140" alt="Generic placeholder image">
-          <h2>Heading</h2>
+          <h2>Contact Info</h2>
           <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
           <p><a class="btn btn-default" href="#row3" role="button">View details &raquo;</a></p>
         </div><!-- /.col-lg-4 -->
