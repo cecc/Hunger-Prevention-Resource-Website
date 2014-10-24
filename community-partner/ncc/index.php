710c9758566a52748f0d32077335fcd75ef43011
CesarOH <cesar.hernandez1094@gmail.com>
2014-10-24 12:08:01 -0400

    Update index.php

--- community-partner/ncc/index.php
+++ community-partner/ncc/index.php
@@ -75,9 +75,8 @@ include ('../../header.php');
     <!-- Wrap the rest of the page in another container to center all the content. -->
 
     <div class="container marketing">
-	<div class="container marketing">
     	<div class="overview" style="margin-bottom:80px;">
-    	<p style="font-size:20px;">In continuation of the university's efforts to address the "digital divide", the mission of the CECC is to support the technological needs of community based organizations and schools. The CECC@NJIT is committed to strengthening the technological capacity of area schools and community-based non-profit organizations. This goal will be accomplished to their needs, but also through education and extended service.</p>
+    	<p style="font-size:20px;">To help residents of inner cities improve the quality of their lives to reflect individual God-given dignity and personal achievement.</p>
 	</div>
       <!-- Three columns of text below the carousel -->
       <div class="row">
